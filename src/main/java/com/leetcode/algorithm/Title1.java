4389f36a6ce0fab2a633304befd618f1c45c1799
chanToTo <38659554+chanToTo@users.noreply.github.com>
2020-02-10 22:49:04 +0800

    leetcode1、3、61、74

---
+++ src/main/java/com/leetcode/algorithm/Title1.java
@@ -0,0 +1,30 @@
+package com.leetcode.algorithm;
+
+/**
+ * @author orangeC
+ * @description 两数之和
+ * @date 2020/2/10 22:24
+ */
+public class Title1 {
+    public static void main(String[] args) {
+        System.out.println("开始：" + System.currentTimeMillis());
+        int[] nums = new int[]{12,34,20,9,50,60,20,50};
+        Title1 numsTitleOne = new Title1();
+        int[] targetInt = numsTitleOne.twoSum(nums,80);
+        System.out.println(targetInt[0] + "," +  targetInt[1]);
+        System.out.println("结束：" + System.currentTimeMillis());
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int count = 0;
+        while(count<nums.length){
+            for(int i=count+1;i<nums.length;i++){
+                if(target == nums[count] + nums[i]){
+                    return new int []{count,i};
+                }
+            }
+            count++;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/leetcode/algorithm/Title3.java
@@ -0,0 +1,42 @@
+package com.leetcode.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author orangeC
+ * @description 无重复字符的最长子串
+ * @date 2020/2/10 22:32
+ */
+public class Title3 {
+    public static void main(String[] args) {
+        String str = "abcdabcdf";
+        getStr(str);
+    }
+
+    private static int getStr(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Set<String> set = new HashSet<>();
+        if ("".equals(s)) {
+            return 0;
+        }
+        int resultLen = 1;
+        for (int i = 0; i < s.length(); i++) {
+            stringBuilder.append(s.charAt(i));
+            set.add(String.valueOf(s.charAt(i)));
+            int len = 1;
+            for (int j = i+1; j < s.length(); j++) {
+                if (!set.add(String.valueOf(s.charAt(j)))){
+                    stringBuilder =  new StringBuilder();
+                    set.clear();
+                    break;
+                } else {
+                    stringBuilder.append(s.charAt(j));
+                }
+                len++;
+                resultLen = Math.max(len, resultLen);
+            }
+        }
+        return resultLen;
+    }
+}
